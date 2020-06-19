0be7c12661836f330e1529f334567388ec248a02
VolhaSM <64163738+VolhaSM@users.noreply.github.com>
2020-06-19 12:00:23 +0300

    messahe Text

---
+++ demo/src/main/java/com/example/demo/controller/response/CountTestResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller.response;
+
+import java.time.LocalDateTime;
+
+
+public class CountTestResponse {
+
+//    private double result;
+//
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM.dd.yyyy")
+//    @JsonSerialize(using = LocalDateTimeSerializer.class)
+//    private LocalDateTime data;
+
+}
